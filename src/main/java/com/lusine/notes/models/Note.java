999bacd1b7ed266ac495daad3634c09ab9030373
Lusine Margaryan <lusine.margarian@gmail.com>
2019-11-10 16:36:22 +0400

    set createdAt and updatedAt at save, and don't allow use to modify them

--- src/main/java/com/lusine/notes/models/Note.java
+++ src/main/java/com/lusine/notes/models/Note.java
@@ -1,15 +1,22 @@
 package com.lusine.notes.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
 
 @Entity
 @Table(name = "notes")
-public class Note {
+public class Note implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    @Column(name="user_id")
     private int userId;
+
     private String title;
     private String note;
 
@@ -18,6 +25,11 @@ public class Note {
 
     @Column(name="updated_at")
     private long updatedAt;
+
+    public Note() {
+        this.createdAt = Instant.now().toEpochMilli();
+        this.updatedAt = Instant.now().toEpochMilli();
+    }
 
     public int getId() {
         return id;
@@ -51,18 +63,22 @@ public class Note {
         this.note = note;
     }
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public long getCreatedAt() {
         return createdAt;
     }
 
+    @JsonIgnore
     public void setCreatedAt(long createdAt) {
         this.createdAt = createdAt;
     }
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public long getUpdatedAt() {
         return updatedAt;
     }
 
+    @JsonIgnore
     public void setUpdatedAt(long updatedAt) {
         this.updatedAt = updatedAt;
     }
