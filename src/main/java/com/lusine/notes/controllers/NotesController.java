1a94141ebacad8ca81caffcb251bb2a41e59cd7b
Lusine Margaryan <lusine.margarian@gmail.com>
2019-11-10 15:40:54 +0400

    getting the note by its Id

--- src/main/java/com/lusine/notes/controllers/NotesController.java
+++ src/main/java/com/lusine/notes/controllers/NotesController.java
@@ -1,15 +1,21 @@
 package com.lusine.notes.controllers;
 
+import com.lusine.notes.models.Note;
+import com.lusine.notes.services.NotesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notes")
 public class NotesController {
+    @Autowired
+    private NotesService notesService;
 
-    @GetMapping("/")
-    public String getAllNotes() {
-        return "Here are all the notes";
+    @GetMapping("/{id}")
+    public Note getNoteById(@PathVariable int id) {
+        return notesService.getNoteById(id);
     }
 }
