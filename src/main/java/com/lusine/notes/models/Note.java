5219807478caac12002eb9b366b3304e6000884d
Lusine Margaryan <lusine.margarian@gmail.com>
2019-11-10 14:44:49 +0400

    initial Note model

---
+++ src/main/java/com/lusine/notes/models/Note.java
@@ -0,0 +1,58 @@
+package com.lusine.notes.models;
+
+public class Note {
+    private int id;
+    private int userId;
+    private String title;
+    private String note;
+    private long createdAt;
+    private long updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
